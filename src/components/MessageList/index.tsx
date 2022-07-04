44d08930132a9ff8c3322338ae3ec191e23640ab
Victor Hugo <victorhugoos@live.com>
2022-07-03 23:11:33 -0300

    environment variable changed to VITE_SERVER_API_URL

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -16,7 +16,7 @@ type Message = {
 
 const messagesQueue: Message[] = []
 
-const socket = io('http://localhost:4000')
+const socket = io(import.meta.env.VITE_SERVER_API_URL)
 
 socket.on('new_message', (newMessage: Message) => {
     messagesQueue.push(newMessage)
